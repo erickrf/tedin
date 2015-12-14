d85a7d58fabee1d04bd4832585344cfd2fd3fc69
Erick Rocha Fonseca <erickrfonseca@gmail.com>
2015-12-14 20:07:07 -0200

    usando stopwords

--- src/config.py
+++ src/config.py
@@ -4,9 +4,11 @@ nlpnet_path_en = r'D:/desenvolvimento/nlpnet/data/dependency'
 nlpnet_path_pt = r'D:/desenvolvimento/nlpnet/data/pos-pt'
 palavras_endpoint = 'http://143.107.183.175:12680/services/service_palavras_flat.php'
 
-lda_path = r'data\lda-100\lda.dat'
-tfidf_path = r'data\lda-100\tfidf.dat'
-tfidf_dict_path = r'data\lda-100\token-dict.dat'
+stopwords_path = 'data/stopwords.txt'
+
+lda_path = r'data/lda-100/lda.dat'
+tfidf_path = r'data/lda-100/tfidf.dat'
+tfidf_dict_path = r'data/lda-100/token-dict.dat'
 
 # URL dentro da rede NILC
 #palavras_endpoint = 'http://10.11.14.126/services/service_palavras_flat.php'
