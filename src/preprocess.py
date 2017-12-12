646ca1701c6c0cf46889d997289d344cccf2dd70
Erick Fonseca <erickrfonseca@gmail.com>
2017-12-12 00:56:34 -0200

    simplified preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,66 +9,38 @@ This is useful when preprocessing takes time.
 import argparse
 import traceback
 import logging
+from six.moves import cPickle
 
 import tokenizer
-import cPickle
 import utils
-import config
 import external
 import datastructures as ds
 
 
-def preprocess_pairs(pairs, parser, binarize):
+def preprocess_pairs(pairs):
     """
-    Preprocess the pairs so we can extract features later on.
+    Preprocess the pairs in-place so we can extract features later on.
 
     :param pairs: list of `Pair` objects
-    :param parser: which parser to call. Currently supports
-        'corenlp', 'malt' and 'palavras'
-    :param binarize: Paraphrases pairs turned into two entailment pairs.
-
     """
-    if parser == 'corenlp':
-        parser_function = external.call_corenlp
-        parser_format = 'conll'
-    elif parser == 'palavras':
-        parser_function = external.call_palavras
-        parser_format = 'palavras'
-    elif parser == 'malt':
-        parser_function = external.call_malt
-        parser_format = 'conll'
-    else:
-        raise ValueError('Unknown parser: %s' % parser)
-
-    inverted_pairs = []
     for i, pair in enumerate(pairs):
         tokens_t = tokenizer.tokenize(pair.t)
         tokens_h = tokenizer.tokenize(pair.h)
 
-        output_t = parser_function(' '.join(tokens_t))
-        output_h = parser_function(' '.join(tokens_h))
+        output_t = external.call_corenlp(' '.join(tokens_t))
+        output_h = external.call_corenlp(' '.join(tokens_h))
 
         try:
-            pair.annotated_t = ds.Sentence(output_t, parser_format)
-            pair.annotated_h = ds.Sentence(output_h, parser_format)
+            pair.annotated_t = ds.Sentence(pair.t, output_t, 'conll')
+            pair.annotated_h = ds.Sentence(pair.h, output_h, 'conll')
         except ValueError as e:
             tb = traceback.format_exc()
             logging.error('Error reading parser output:', e)
             logging.error(tb)
 
         utils.find_lexical_alignments(pair)
-        if binarize:
-            if pair.entailment == ds.Entailment.paraphrase:
-                pair.entailment = ds.Entailment.entailment
-                inverted = pair.inverted_pair(ds.Entailment.entailment)
-            else:
-                inverted = pair.inverted_pair(ds.Entailment.none)
-
-            inverted_pairs.append(inverted)
 
         pairs[i] = pair
-
-    pairs.extend(inverted_pairs)
 
 
 if __name__ == '__main__':
@@ -76,12 +48,10 @@ if __name__ == '__main__':
     parser.add_argument('input', help='XML file with pairs')
     parser.add_argument('output',
                         help='File to write output file (pickle format)')
-    parser.add_argument('-b', action='store_true', dest='binarize',
-                        help='Binarize (turn paraphrases into two entailments)')
     args = parser.parse_args()
 
     pairs = utils.read_xml(args.input)
-    preprocess_pairs(pairs, config.parser, args.binarize)
+    preprocess_pairs(pairs)
     with open(args.output, 'wb') as f:
         cPickle.dump(pairs, f)
 
